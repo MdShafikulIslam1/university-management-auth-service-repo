531d32101fa308159158a67f8594fe6d2df2516b
shafikul-islam <179398ssshofik@gmail.com>
2023-06-08 20:55:30 +0600

    testing purpose

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("Database connect successfully");
+    await mongoose.connect(config.database_url as string)
+    console.log('Database connect successfully')
     app.listen(config.port, () => {
       console.log(
         `University Management Application listening on port ${config.port}`
-      );
-    });
+      )
+    })
   } catch (error) {
-    console.log("failed to connect database", error);
+    console.log('failed to connect database', error)
   }
 }
-bootstrap();
+bootstrap()
