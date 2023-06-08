b55ffe7ad884207dcf87f62551a6c4340718e3f9
shafikul-islam <179398ssshofik@gmail.com>
2023-06-08 22:21:27 +0600

    [DONE]create user interface and model

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 //test api
-app.get('/', (req: Request, res: any) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('working properly')
 })
 
